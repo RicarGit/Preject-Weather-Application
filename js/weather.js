0de8fe9715426d5f9d8115f490898cc01cc33f36
Ricardo Gava <Ricardoagava@gmail.com>
2021-06-06 00:39:32 -0300

    extracted all the API infos i needed on the new js file named weather.js to use them in the future

---
+++ js/weather.js
@@ -0,0 +1,46 @@
+const APIKey = 'SBMSWwMZP03GxVDtWSticiY92rGo4NT8'
+const userInput = 'Itatiba'
+
+const getLocationURL = userInput => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${userInput}&language=pt-br`
+
+const getLocationKey = async userInput => {
+  try {
+    const response = await fetch(getLocationURL(userInput))
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados.')
+    }
+
+    const [locationData] = await response.json()
+
+    return locationData.Key
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+}
+
+const getCurrentConditionsURL = locationKey => `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${APIKey}`
+
+const getCityCurrentConditions = async () => {
+  const locationKey = await getLocationKey(userInput)
+  try {
+    const response = await fetch(getCurrentConditionsURL(locationKey))
+
+    if (!response.ok) {
+      throw new Error('Não foi possível obter os dados.')
+    }
+
+    const [conditionsData] = await response.json()
+
+    const isDayTime = conditionsData.IsDayTime
+    const weatherText = conditionsData.WeatherText
+    const cityCelsiusTemperature = conditionsData.Temperature.Metric.Value
+
+  } catch ({ name, message }) {
+    alert(`${name}: ${message}`)
+  }
+
+}
+
+getLocationKey(userInput)
+getCityCurrentConditions()
