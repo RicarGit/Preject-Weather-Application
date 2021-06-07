a4544c728a4871719fb2898ffb08c6d862de8d71
Ricardo Gava <Ricardoagava@gmail.com>
2021-06-06 22:39:11 -0300

    refatored getCityURL to receive key number, not the object like before

--- js/weather.js
+++ js/weather.js
@@ -4,8 +4,8 @@ const defaultURL = 'http://dataservice.accuweather.com/'
 const getCityURL = cityName =>
   `${defaultURL}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
 
-const getWeatherURL = ({ Key }) =>
-  `${defaultURL}currentconditions/v1/${Key}?apikey=${APIKey}&language=pt-br`
+const getWeatherURL = cityKey =>
+  `${defaultURL}currentconditions/v1/${cityKey}?apikey=${APIKey}&language=pt-br`
 
 const fetchData = async url => {
   try {
@@ -23,9 +23,4 @@ const fetchData = async url => {
 
 const getCityData = cityName => fetchData(getCityURL(cityName))
 
-const getCityCurrentConditions = async cityName => {
-  const [cityData] = await getCityData(cityName)
-  return fetchData(getWeatherURL(cityData))
-}
-
-getCityCurrentConditions('itatiba')
+const getCityCurrentConditions = cityKey => fetchData(getWeatherURL(cityKey))
