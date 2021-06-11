198d9548c6c67820b6e7297ba9a7a599d6b8827d
Ricardo Gava <Ricardoagava@gmail.com>
2021-06-10 21:53:57 -0300

    refatored the weather.js, added the city name to localStorage to save the last user visit, now the background color change according to the daytime

--- js/app.js
+++ js/app.js
@@ -8,58 +8,71 @@ const dayTimeContainer = document.querySelector('[data-js="time"]')
 const timeIconContainer = document
   .querySelector('[data-js="time-icon"]')
 
-const img = document.createElement('img')
-const regex = /^.{3,}$/
-
-const insertWeatherInfoIntoDom = (element, info) => element.textContent = info
+const imgElement = document.createElement('img')
+const limitInputToThreeOrMoreCharacters = /^.{3,}$/
 
 const getWeatherIconSrc = iconID => `./src/icons/${iconID}.svg`
 
-const insertImageIconIntoDom = iconID => {
-  img.src = getWeatherIconSrc(iconID)
-  timeIconContainer.insertAdjacentElement('afterbegin', img)
+const insertImageIconIntoWeatherCard = iconID => {
+  imgElement.src = getWeatherIconSrc(iconID)
+  timeIconContainer.insertAdjacentElement('afterbegin', imgElement)
 }
 
 const showCityCard = cityCard => {
-  cityCard.classList.contains('d-none')
-    ? cityCard.classList.remove('d-none')
-    : null
+  const contaisClassDNone = cityCard.classList.contains('d-none')
+  if (contaisClassDNone) {
+    cityCard.classList.remove('d-none')
+  }
 }
 
-const insertDayTimeImage = isDayTime =>
+const showCityDayTime = isDayTime => {
   dayTimeContainer.src = isDayTime ? './src/day.svg' : './src/night.svg'
+  document.body.classList = isDayTime ? 'color: bg-primary' : 'color: bg-dark'
+}
 
 const clearFormInput = () => {
   cityForm.reset()
   cityForm.focus()
 }
 
-displayCityWeatherInfos = async event => {
+const getCityWeatherInfos = async cityName => {
+  const [{ Key, LocalizedName }] = await
+    getCityData(cityName)
+  const [{ IsDayTime, WeatherText, Temperature, WeatherIcon }] = await
+    getCityCurrentConditions(Key)
+
+  const cityCelsiusTemperature = Math.round(Temperature.Metric.Value)
+
+  cityNameContainer.textContent = LocalizedName
+  weatherClimate.textContent = WeatherText
+  weatherTemperature.textContent = cityCelsiusTemperature
+
+  showCityCard(cityCard)
+  showCityDayTime(IsDayTime)
+  insertImageIconIntoWeatherCard(WeatherIcon)
+}
+
+const displayCityWeatherInfos = event => {
   event.preventDefault()
+
   const inputValue = event.target.city.value.trim()
-  const isValidInput = regex.test(inputValue)
+  const isValidInput = limitInputToThreeOrMoreCharacters.test(inputValue)
 
   if (isValidInput) {
-    showCityCard(cityCard)
-
-    const [{ Key, LocalizedName, AdministrativeArea }] = await
-      getCityData(inputValue)
-    const [{ IsDayTime, WeatherText, Temperature, WeatherIcon }] = await
-      getCityCurrentConditions(Key)
-
-    const cityName = `${LocalizedName} / ${AdministrativeArea.ID}`
-    const isDayTime = IsDayTime
-    const cityClimate = WeatherText
-    const cityCelsiusTemperature = Math.round(Temperature.Metric.Value)
-
-    insertDayTimeImage(isDayTime)
-    insertImageIconIntoDom(WeatherIcon)
-    insertWeatherInfoIntoDom(cityNameContainer, cityName)
-    insertWeatherInfoIntoDom(weatherClimate, cityClimate)
-    insertWeatherInfoIntoDom(weatherTemperature, cityCelsiusTemperature)
+    getCityWeatherInfos(inputValue)
+    localStorage.setItem('cityName', inputValue)
   }
 
   clearFormInput()
 }
 
+const showCityInLocalStorage = () => {
+  const localStorageCityName = localStorage.getItem('cityName')
+
+  if (localStorageCityName) {
+    getCityWeatherInfos(localStorageCityName)
+  }
+}
+
+showCityInLocalStorage()
 cityForm.addEventListener('submit', displayCityWeatherInfos)
